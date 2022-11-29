95942c2de4f3c2d7cf7670f71c2183ce60ab3d7a
r-y-a-n-m-a-r-t-i-n <ryanlmartin2k@gmail.com>
2022-11-28 20:52:34 -0500

    started migrating drake over

--- slip_test_drake_ryan.py
+++ slip_test_drake_ryan.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pydrake.solvers.mathematicalprogram import MathematicalProgram
 
 
 class Robot:
@@ -48,15 +49,15 @@ class Robot:
         )
 
         F_s = (
-            self.k
-            * (self.L_0 - d + q)
-            * np.array(
-                [
-                    (p_com[0] - p_foot[0]) / d,
-                    (p_com[1] - p_foot[1]) / d,
-                    (p_com[2] - p_foot[2]) / d,
-                ]
-            )
+                self.k
+                * (self.L_0 - d + q)
+                * np.array(
+            [
+                (p_com[0] - p_foot[0]) / d,
+                (p_com[1] - p_foot[1]) / d,
+                (p_com[2] - p_foot[2]) / d,
+            ]
+        )
         )
 
         # print((p_com[0] - p_foot[0]) / d)
@@ -76,10 +77,54 @@ class Robot:
     def step(self, x, u, dt):
         self.x += self.f(x, u) * dt
 
+    def CollocationConstraintEvaluator(planar_arm, context, dt, x_i, u_i, x_ip1, u_ip1):
+        n_x = planar_arm.num_positions() + planar_arm.num_velocities()
+        h_i = np.zeros(n_x, )
+        # TODO: Add f_i and f_ip1
+        # You should make use of the EvaluateDynamics() function to compute f(x,u)
+        s0 = x_i
+        f_i = robot.f(x_i, u_i)
+        s1 = f_i
+        f_ip1 = robot.f(x_ip1, u_ip1)
+        s2 = (3 * x_ip1 - 3 * x_i - 2 * f_i * dt - f_ip1 * dt) / (dt * dt)
+        s3 = (-2 * x_ip1 + 2 * x_i + f_i * dt + f_ip1 * dt) / (dt * dt * dt)
+
+        s_midpoint = (1 / 2) * (x_i + x_ip1) - (dt / 8) * (f_ip1 - f_i)
+        sdot_midpoint = 3 / (2 * dt) * (x_ip1 - x_i) - 0.25 * (f_i + f_ip1)
+
+        h_i = sdot_midpoint - robot.f(s_midpoint, (u_i + u_ip1) * 0.5)
+
+        return h_i
+
+    def AddCollocationConstraints(prog, planar_arm, context, N, x, u, timesteps):
+        n_u = planar_arm.num_actuators()
+        n_x = planar_arm.num_positions() + planar_arm.num_velocities()
+
+        for i in range(N - 1):
+            def CollocationConstraintHelper(vars):
+                x_i = vars[:n_x]
+                u_i = vars[n_x:n_x + n_u]
+                x_ip1 = vars[n_x + n_u: 2 * n_x + n_u]
+                u_ip1 = vars[-n_u:]
+                return robot.CollocationConstraintEvaluator(planar_arm, context, timesteps[i + 1] - timesteps[i], x_i, u_i,
+                                                      x_ip1,
+                                                      u_ip1)
+
+            # TODO: Within this loop add the dynamics constraints for segment i (aka collocation constraints)
+            #       to prog
+            # Hint: use prog.AddConstraint(CollocationConstraintHelper, lb, ub, vars)
+            # where vars = hstack(x[i], u[i], ...)
+            lb = np.zeros(n_x)
+            ub = lb
+            vars = np.hstack((x[i], u[i], x[i + 1], u[i + 1]))
+            prog.AddConstraint(CollocationConstraintHelper, lb, ub, vars)
+
 
 if __name__ == "__main__":
     # Instantiate robot
     robot = Robot()
+
+    prog = MathematicalProgram()
 
     # Simulation parameters
     dt = 0.0001
